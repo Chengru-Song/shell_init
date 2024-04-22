484a5a424043038b70cc472e3905c5136fe666d8
Chengru-Song <260928310@qq.com>
2024-04-22 16:39:40 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ sudo apt-get install zsh
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 pip install powerline-status --user
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git\
 	zsh-autosuggestions\
 	zsh-syntax-highlighting)/' ~/.zshrc
